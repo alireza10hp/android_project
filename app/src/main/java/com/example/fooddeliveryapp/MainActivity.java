abd97a00bab0227aed24cfabb0f7cff49e76a7e0
mahdi <mahdifarzadi78@gmail.com>
2023-02-03 03:15:50 +0330

    add login and register activities

---
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.fooddeliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn_login;
+    Button btn_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
+
+        btn_login.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)));
+        btn_register.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, RegisterActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)));
+    }
+}
